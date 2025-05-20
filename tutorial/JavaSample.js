f72970181e2a7ed7404512d687dba48df5b06e8f
PerryLeperi <perry.a.leperi@gmail.com>
2025-05-19 19:46:49 -0500

    Aunt Esters No Good Recipes

---
+++ tutorial/JavaSample.js
@@ -0,0 +1,7 @@
+function askUserName() {
+var username = prompt("What is your name?");
+alert ("Hello "+ username);
+var header1 = document.getElementById("header1");
+header1.innerHTML = username;
+}
+
